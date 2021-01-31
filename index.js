9b099d43b442149b8fa4e05b415e52fb7806b188
phhu <phhu.git@gmail.com>
2021-01-31 22:27:23 +0000

    four bar

--- index.js
+++ index.js
@@ -1,145 +1,49 @@
-//import * as Tone from 'tone';
 import './style.css';
 
-//const core2 = require('@magenta/music/node/core');
-//const mvae = require('@magenta/music/node/music_vae');
-//core2.Recorder
-//core2.StaffSVGVisualizer
-
-//create a synth and connect it to the main output (your speakers)
-
-const {MELODY1,TWINKLE_TWINKLE} = require('./melodies');
+const {MELODY1,MELODY2} = require('./melodies');
 const {sfPlayer} = require('./sfPiano');
-const {recorder} = require('./recorder');
-const {sequencesMatch} = require('./compareSequences');
 const seq = core.sequences;
-/*
-// https://tonejs.github.io/examples/polySynth
-const synth = new Tone.PolySynth().toDestination();
-
-const makeNoise = () => {
-  console.log("makeNoise");
-  synth.triggerAttackRelease("C4", "8n"); 
-  synth.triggerAttackRelease("E4", "8n"); 
-  synth.triggerAttackRelease("G4", "4n"); 
-} 
-*/
 
 const makeButton = ({
   id,
   parent = "controls",
   type = "button",
-  fn = () =>console.log("clicked"),
+  fn = ()=> () =>console.log("clicked"),
 }={}) => {
-  if (type==="br"){
-    const br = document.createElement('br');
-    document.getElementById(parent).appendChild(br);  
-  } else {
-    const b = document.createElement('button');
-    b.setAttribute('id',id);
-    b.innerHTML = id;
-    document.getElementById(parent).appendChild(b);
-    document.getElementById(id).addEventListener('click',fn);   
-  }
+  const b = document.createElement('button');
+  b.setAttribute('id',id);
+  b.innerHTML = id;
+  document.getElementById(parent).appendChild(b);
+  console.log("adding Click",id,fn,document.getElementById(id));
+  document.getElementById(id).addEventListener('click',fn());
 }
+
 //control mapping
-[
-  {parent: "active",  id: "play source", fn: ()=>{playMelody(activeMelody)} },
-  {parent: "sample1", id: "play variant 1", fn: ()=>{playMelody(samples[0])} },  
-  {parent: "sample2", id: "play variant 2", fn: ()=>{playMelody(samples[1])} },  
-  {parent: "sample3", id: "play variant 3", fn: ()=>{playMelody(samples[2])} }, 
-  {parent: "active", id: "makeSimilar",  fn: ()=>{ makeSimilar({count:3})(activeMelody) } },  
-  {parent: "sample1",id: "choose variant 1", fn: ()=>{chooseMelody(samples[0])} },  
-  {parent: "sample2",id: "choose variant 2", fn: ()=>{chooseMelody(samples[1])} },  
-  {parent: "sample3",id: "choose variant 3", fn: ()=>{chooseMelody(samples[2])} }, 
-  //{id: "start",fn: ()=>Tone.start() },
-  //{id: "playMidi",fn: ()=>midiPlayer.start(activeMelody) },
-  {parent:"afterRecording",id: "RECORD (midi)",fn: ()=>{
-    recorder.callbackObject = null;
-    recorder.enablePlayClick(
-      document.getElementById("useClick").checked
-    );
-    recorder.start();
-    console.log("recording start"); 
-    document.getElementById("RECORD (midi)").style['background-color'] =  'red'
-  }},
-  {parent:"afterRecording",id: "Stop REC",fn: ()=>{
-    let rec = recorder.stop();
-    document.getElementById("RECORD (midi)").style['background-color'] =  'inherit'
-    console.log("recording stop",rec);
-    const stepsPerBeat = 4;
-    //const stepsPerBar = 4 * stepsPerBeat;
-    const startTime = rec.notes[0] && rec.notes[0].startTime ;
-    const endTime = rec.notes[0] && rec.notes[rec.notes.length-1].endTime ;
-    rec = seq.trim(rec,startTime,endTime);
-    rec = seq.quantizeNoteSequence(rec,stepsPerBeat);
-    console.log("recording quantized and trimmed:",rec);
-    recording = rec;    
-    showRecording(rec);
-    testRecordings();
-  }},
-  {parent:"afterRecording",id:"Play recording",   fn: () => {playMelody(recording);testRecordings();}},
-  {parent:"afterRecording",id:"Choose recording", fn: () => {chooseMelody(recording);}},
-  //{parent:"afterRecording",id:"testRec",   fn: () => testRecordings()}
-  {parent: "active",id:"twinkle",   fn: () => chooseMelody(TWINKLE_TWINKLE)},
-  {parent: "active",id:"melody1",   fn: () => chooseMelody(MELODY1)},
-].map(makeButton);
+const buttonSpecs = [
+  {parent: "melody1",  id: "play1", fn: ()=>()=>{playMelody(MELODY1)} },
+  {parent: "melody2",  id: "play2", fn: ()=>()=>{playMelody(MELODY2)} },
+  {parent: "controls",  id: "inter", fn: ()=>()=>{reinterpolate() }   },
+];
+buttonSpecs.map(makeButton);
 
-let recording;
-
-const setStaffColor = (id,comparison) => {
-  const green = "rgba(0, 252, 0, 0.178)";
-  const match = sequencesMatch(recording,comparison);
-  document.getElementById(id).style['background-color'] = match ? green : 'inherit';
-  console.log(id,match);
-}
-
-const clearRecordingTest = () => {
-  ['staff0','staff1','staff2','staff3']
-    .forEach(id => 
-      document.getElementById(id).style['background-color'] =  'inherit'
-    )
-};
-
-const testRecordings = () => {
-  if(recording){
-    setStaffColor('staff0',activeMelody);
-    setStaffColor('staff1',samples[0]);
-    setStaffColor('staff2',samples[1]);
-    setStaffColor('staff3',samples[2]);
-  } else {
-    console.log("no recording to test!");
-  }
-};
-
-const showRecording = (rec) => {
-  visualizeMelody({n:"Rec"})(rec);
-} 
 
 const src = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small';  // 'data/mel_small'
 const model = new music_vae.MusicVAE(src);
 const player = new core.Player();    // WithClick
 const midiPlayer = new core.MIDIPlayer();
 midiPlayer.requestMIDIAccess().then((x) => {
-  // For example, use only the first port. If you omit this,
-  // a message will be sent to all ports.
-  //midiPlayer.outputs = [midiPlayer.availableOutputs[0]];
   midiPlayer.outputs = x;
   midiPlayer.outputChannel = 1;
-  //console.log("playing via midi",x,midiPlayer.availableOutputs);
-  //midiPlayer.resumeContext();
-  //midiPlayer.start(MELODY1).then(()=>console.log("midiPlayer started"));
 })
-
 
 const prepare = Promise.all([
   model.initialize(),
-  recorder.initialize(),
+  //recorder.initialize(),
   //sfPlayer.loadAllSamples(),
 ]);
 
-let samples;
-let activeMelody;
+global.samples = [];
+//let melodies = [];
 
 const log = (msg) => {
   document.getElementById('status').innerHTML = msg;
@@ -150,26 +54,47 @@ log("preparing. please wait");
 prepare.then((_) => {
   log("ready");
   //makeSamples();
-  chooseMelody(TWINKLE_TWINKLE);
-  sfPlayer.loadAllSamples().then(()=>log("loaded soundfont samples"));
+  setupMelodies(MELODY1,MELODY2);
+  //sfPlayer.loadAllSamples().then(()=>log("loaded soundfont samples"));
   //chooseMelody(MELODY1);
   //makeSimilar({count:3})(activeMelody)
 });
 
-const makeSamples = melody => {
-  model.sample(2)
-  .then(newSamples => {
-    console.log("made samples");
-    samples = newSamples;
-  });
-}
+const getInterpolationCount = () => 10;
 
-const makeSimilar = ({count=3}={}) => melody => {
-  model.similar(melody,count,0.75)
+
+const reinterpolate = () => {
+  console.log("reinterpolating");
+  interpolateMelodies()(MELODY1,MELODY2);
+}
+global.reinterpolate = reinterpolate;
+
+const interpolateMelodies = ({
+  count=3
+}={}) => (mel1,mel2) => {
+  /* interpolate(
+      inputSequences: INoteSequence[], 
+      numInterps: number | number[], 
+      temperature?: number, 
+      controlArgs?: MusicVAEControlArgs
+    ): Promise<INoteSequence[]>
+  */
+  model.interpolate([mel1,mel2],getInterpolationCount(),0.5)
   .then(newMelodies => {
-    console.log("made similar",newMelodies);
-    samples = newMelodies;
-    samples.forEach((s,i)=>visualizeMelody({n:i+1})(s))
+    console.log("interpolated:",newMelodies);
+    global.samples = newMelodies;
+    const d = document.getElementById("interpolatedMelodies")
+    d.innerHTML="";
+    global.samples.forEach((s,i)=>{
+      d.innerHTML+=`<div id="staffInt${i}"></div>`;
+      d.innerHTML+=`<button id="buttonInt${i}" onclick="console.log('playing Int${i}');playMelody(samples[${i}]);">Play Int ${i}</button>`;
+      visualizeMelody({scoreDiv:"staffInt",n:i})(s);
+      /*makeButton({
+        id: `Play_Int_${i}`,
+        parent: `staffInt${i}`,
+        fn: (() => () => {log("clicked "+ i)}),
+      });*/
+    });
   });
 }
 
@@ -189,31 +114,24 @@ const playMelody = melody => {
   //console.log(samples[0]);
   console.log("playing",melody);
 }
+global.playMelody = playMelody;
 
-
-
-const chooseMelody = melody => {
-  activeMelody = melody;
-  visualizeMelody()(melody);
-  makeSimilar()(activeMelody);
-  clearRecordingTest();
-  return melody;
+const setupMelodies = (mel1,mel2) => {
+  visualizeMelody({scoreDiv:'staffMelody',n:1})(mel1);
+  visualizeMelody({scoreDiv:'staffMelody',n:2})(mel2);
+  interpolateMelodies()(mel1,mel2);
+  //return melody;
 };
-global.chooseMelody = chooseMelody;
-
-let staff;
-let roll;
-let waterfall;
 
 const visualizeMelody = ({
   scoreDiv="staff",
   rollDiv="roll",
   waterfallDiv="waterfall",
   n="0",
-}={}) => melody => {
-  //console.log("vis",n)
-  staff = new core.StaffSVGVisualizer(melody, document.getElementById(scoreDiv + n ));
-  //roll = new core.PianoRollSVGVisualizer(melody, document.getElementById(rollDiv));
-  //waterfall = new core.WaterfallSVGVisualizer(melody, document.getElementById(waterfallDiv));
+}={}) => melody => { 
+  console.log("vis",n)
+  const staff = new core.StaffSVGVisualizer(melody, document.getElementById(scoreDiv + n ));
+  //const roll = new core.PianoRollSVGVisualizer(melody, document.getElementById(rollDiv));
+  //const waterfall = new core.WaterfallSVGVisualizer(melody, document.getElementById(waterfallDiv));
 }
 
--- melodies.js
+++ melodies.js
@@ -9,7 +9,7 @@ function toMidi(note) {
 }
 
 
-const MELODY1 = { 
+const MELODY2 = { 
   totalQuantizedSteps: 32,
   quantizationInfo:{stepsPerQuarter: 4},
   notes: [
@@ -27,7 +27,50 @@ const MELODY1 = {
   ]
 };
 
-const TWINKLE_TWINKLE = {
+const LT = { 
+  totalQuantizedSteps: 62,
+  quantizationInfo:{stepsPerQuarter: 4},
+  notes: [
+    {pitch: toMidi('A3'), quantizedStartStep: 0, quantizedEndStep: 4},
+    //{pitch: toMidi('C3'), quantizedStartStep: 0, quantizedEndStep: 32},
+    {pitch: toMidi('D4'), quantizedStartStep: 4, quantizedEndStep: 6},
+    {pitch: toMidi('E4'), quantizedStartStep: 6, quantizedEndStep: 8},
+    {pitch: toMidi('F4'), quantizedStartStep: 8, quantizedEndStep: 10},
+    {pitch: toMidi('D4'), quantizedStartStep: 10, quantizedEndStep: 12},
+    {pitch: toMidi('E4'), quantizedStartStep: 12, quantizedEndStep: 16},
+    {pitch: toMidi('C4'), quantizedStartStep: 16, quantizedEndStep: 20},
+    {pitch: toMidi('D4'), quantizedStartStep: 20, quantizedEndStep: 26},
+    {pitch: toMidi('A3'), quantizedStartStep: 26, quantizedEndStep: 28},
+    {pitch: toMidi('A3'), quantizedStartStep: 28, quantizedEndStep: 32}
+  ]
+};
+
+
+
+const s = 16;
+const MELODY1 = { 
+  totalQuantizedSteps: 32,
+  quantizationInfo:{stepsPerQuarter: 4},
+  notes: [
+    {pitch: 60, quantizedStartStep: 0, quantizedEndStep: 2},
+    {pitch: 60, quantizedStartStep: 2, quantizedEndStep: 4},
+    {pitch: 60, quantizedStartStep: 4, quantizedEndStep: 6},
+    {pitch: 60, quantizedStartStep: 6, quantizedEndStep: 7},
+    {pitch: 60, quantizedStartStep: 7, quantizedEndStep: 8},
+    {pitch: 60, quantizedStartStep: 8, quantizedEndStep: 10},
+    {pitch: 55, quantizedStartStep: 10, quantizedEndStep: 12},
+
+    {pitch: 60, quantizedStartStep: s+0, quantizedEndStep: s+2},
+    {pitch: 60, quantizedStartStep: s+2, quantizedEndStep: s+4},
+    {pitch: 60, quantizedStartStep: s+4, quantizedEndStep: s+6},
+    {pitch: 60, quantizedStartStep: s+6, quantizedEndStep: s+7},
+    {pitch: 60, quantizedStartStep: s+7, quantizedEndStep: s+8},
+    {pitch: 60, quantizedStartStep: s+8, quantizedEndStep: s+10},
+    {pitch: 55, quantizedStartStep: s+10, quantizedEndStep: s+12},
+  ]
+};
+
+const MELODY3 = {
   totalQuantizedSteps: 32,
   quantizationInfo:{stepsPerQuarter: 4},
   notes: [
@@ -52,5 +95,5 @@ const TWINKLE_TWINKLE = {
 
 module.exports = {
   MELODY1, 
-  TWINKLE_TWINKLE,
+  MELODY2,
 };
